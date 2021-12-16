257efa8feeaef416e2d6375c26f06f019f1c21d0
Marcos Antonio Gonçalves <58779341+compromitto@users.noreply.github.com>
2021-12-16 00:38:30 -0300

    update gerarValor
    Gerar valor com numero generico

--- src/components/GeraValor.js
+++ src/components/GeraValor.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Form, Card, Button, Alert} from "react-bootstrap"
+import { Form, Card, Button, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
 
